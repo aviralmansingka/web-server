312e2ec727ad23ba7ec3724663045d1973019480
Aviral Mansingka <aviralmansingka@gmail.com>
2016-04-08 23:26:09 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+
+var middleware = {
+	requireAuthentication : function(req, res, next) {
+		console.log('Private log hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' on ' + new Date().toString());
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+	console.log('Express Server started on port ' + port + '!');
+});
